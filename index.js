d3f3a76a8f1bbaa2f64b0cbf1ef0df5a0c07ea21
Aleksey <fduchunru@gmail.com>
2020-09-29 00:59:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 destructivelyUpdateObjectWithKeyAndValue(recipes, 'sugar', '5 tbsp')
 
 function deleteFromObjectByKey(object, key){
-  delete object.key
+  delete object[key]
   return object
 }
 deleteFromObjectByKey(recipes, 'flour')
