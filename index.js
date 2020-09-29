f3f0699d61ba1bae50fa55a90207d9c593b3b5f3
Aleksey <fduchunru@gmail.com>
2020-09-29 00:36:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, {[key]: value})
 }
 updateObjectWithKeyAndValue(recipes, 'milk', '1 cup')
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  return object.key = ["value"]
+}
+destructivelyUpdateObjectWithKeyAndValue(recipes, 'sugar', '5 tbsp')
